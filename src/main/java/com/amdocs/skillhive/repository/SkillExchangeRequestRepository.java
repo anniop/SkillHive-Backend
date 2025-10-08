d9ccbaf606da6c77b411d532996efcdecae0f790
Aniket Mogal <anikemog@amdocs.com>
2025-10-08 15:41:35 +0530

    Code

--- src/main/java/com/amdocs/skillhive/repository/SkillExchangeRequestRepository.java
+++ src/main/java/com/amdocs/skillhive/repository/SkillExchangeRequestRepository.java
@@ -1,14 +1,16 @@
 package com.amdocs.skillhive.repository;
 
-import com.amdocs.skillhive.model.RequestStatus;
 import com.amdocs.skillhive.model.SkillExchangeRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.amdocs.skillhive.model.RequestStatus;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface SkillExchangeRequestRepository extends JpaRepository<SkillExchangeRequest, Integer> {
+    // keep it simple so Spring Data can create the repository bean
     List<SkillExchangeRequest> findBySender_UserId(Integer senderId);
     List<SkillExchangeRequest> findByReceiver_UserId(Integer receiverId);
     List<SkillExchangeRequest> findByStatus(RequestStatus status);
