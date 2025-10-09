b6b63c482146b629d2bae07961c4b69801ebfa06
Anni0p <b.c114303@gmail.com>
2025-10-09 10:23:51 +0530

    code

---
+++ src/main/java/com/amdocs/skillhive/SkillhiveApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.skillhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.amdocs.skillhive")
+public class SkillhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillhiveApplication.class, args);
+	}
+
+}
